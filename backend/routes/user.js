40a39982f9f79bca7bf3ee6a872ac384aae6e753
Nicolas9118 <rolland.nicolas9118@outlook.com>
2022-07-06 10:21:41 +0200

    identification (avec problèmes)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const userCtrl = require("../controllers/userconnect");
 
-router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+const auth = require("../middleware/auth");
+const userCtrl = require("../controllers/user");
+
+router.post("/signup", auth, userCtrl.signup);
+router.post("/login", auth, userCtrl.login);
 
 module.exports = router;

f4f07b6296c577f2477b81f51d95ed779b2272a2
Nicolas9118 <rolland.nicolas9118@outlook.com>
2022-07-07 11:03:01 +0200

    signup et login créer

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require("../middleware/auth");
+const authorize = require("../middleware/authorize");
 const userCtrl = require("../controllers/user");
 
-router.post("/signup", auth, userCtrl.signup);
-router.post("/login", auth, userCtrl.login);
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
 
 module.exports = router;
